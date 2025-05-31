874d9c0a61106f489dfa11c329c4f842dadfb16f
Damanrakwal47 <119044441+Damanrakwal47@users.noreply.github.com>
2025-06-01 03:19:39 +0530

    Update index.js yeah

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://place-net-campus-connect-g54k.vercel.app",
+      "https://place-net-campus-connect-9gwr.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
